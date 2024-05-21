5c65f3553756835a4c5d9e1a960ad38cb2e52c6b
Pierlou <pierlou.ramade@data.gouv.fr>
2024-05-21 17:20:53 +0200

    docs: better logs

--- process_pnt.py
+++ process_pnt.py
@@ -14,14 +14,15 @@ from utils import (
 )
 
 logging.basicConfig(level=logging.INFO)
+cooldown = 60
 
 
 if __name__ == "__main__":
     while True:
 
-        logging.info("-------------------------------------")
-        logging.info(f"------  NEW PROCESS {datetime.now().strftime('%Y-%m-%dT%H:%M')}  ------")
-        logging.info("-------------------------------------")
+        logging.info("--------------------------------------")
+        logging.info(f"---  NEW PROCESS {datetime.now().strftime('%Y-%m-%dT%H:%M')}  ---")
+        logging.info("--------------------------------------")
 
         result = None
         list_files = None
@@ -43,7 +44,9 @@ if __name__ == "__main__":
             result = construct_all_possible_files(batches, tested_batches)
         except TypeError as e:
             result = None
-            logging.info(f"Error - wait next batch: {e}")
+            logging.warning(f"Error constructing files: {e}")
+            logging.info(f"Restarting a process in {cooldown}s")
+            time.sleep(cooldown)
             continue
 
         if result is not None:
@@ -53,7 +56,9 @@ if __name__ == "__main__":
             logging.info("---- Processing each possible file ----")
             processing_each_possible_files(meta_urls, current_folder, family_batches)
         except Exception as e:
-            logging.info(f"EXCEPTION: {e}")
+            logging.warning(f"Error processing all file combinations: {e}")
+            logging.info(f"Restarting a process in {cooldown}s")
+            time.sleep(cooldown)
             continue
 
         try:
@@ -64,7 +69,9 @@ if __name__ == "__main__":
                 logging.info("---- Reorder resources of data.gouv for each dataset ----")
                 reorder_resources(ctx)
         except Exception as e:
-            logging.info(f"Problem occured during publishing in data.gouv: {e}")
+            logging.warning(f"Problem occured during publishing in data.gouv: {e}")
+            logging.info(f"Restarting a process in {cooldown}s")
+            time.sleep(cooldown)
             continue
 
         logging.info("---- Remove files in minio and data.gouv.fr if more than MAX BATCH SIZE ----")
@@ -76,4 +83,4 @@ if __name__ == "__main__":
         logging.info("------       END PROCESS       ------")
         logging.info("-")
 
-        time.sleep(60)
+        time.sleep(cooldown)
