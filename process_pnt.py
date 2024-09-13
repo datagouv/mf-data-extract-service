176bc81e32d2a2de06fd0e7f28e3ac1e1562ebad
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2024-09-13 11:59:20 +0200

    fix: skip upload tree if nothing changed (#13)

--- process_pnt.py
+++ process_pnt.py
@@ -79,8 +79,9 @@ if __name__ == "__main__":
             time.sleep(cooldown)
             continue
 
-        logging.info("---- Getting file tree on minio and update it on data.gouv ----")
-        dump_and_send_tree()
+        if not skip:
+            logging.info("---- Getting file tree on minio and update it on data.gouv ----")
+            dump_and_send_tree()
 
         logging.info("---- Remove files in minio and data.gouv.fr if more than MAX BATCH SIZE ----")
         clean_old_runs_in_minio(batches)
